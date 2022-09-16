048bc4b21376e129e8d2b53d0b8a8db2573d0869
subnub <kyle.hoell@gmail.com>
2022-09-16 17:03:30 -0400

    fixed file renaming error, also improved styles

--- components/Message/Item.vue
+++ components/Message/Item.vue
@@ -7,10 +7,22 @@
       <button class="mr-3" @click="setMessageAsStatus" :disabled="loading">
         Set as status
       </button>
-      <button class="mr-3" @click="$router.push(`edit/${messageItem.id}`)">
-        Edit
-      </button>
-      <button @click="removeMessage">Remove</button>
+      <svg
+        @click="$router.push(`edit/${messageItem.id}`)"
+        class="delete-icon mx-2"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M19,3A2,2 0 0,1 21,5V19C21,20.11 20.1,21 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M16.7,9.35C16.92,9.14 16.92,8.79 16.7,8.58L15.42,7.3C15.21,7.08 14.86,7.08 14.65,7.3L13.65,8.3L15.7,10.35L16.7,9.35M7,14.94V17H9.06L15.12,10.94L13.06,8.88L7,14.94Z"
+        />
+      </svg>
+      <svg @click="removeMessage" class="edit-icon" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+        />
+      </svg>
     </div>
   </div>
 </template>
